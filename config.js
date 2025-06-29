f1cca88d48f89d4ad08c03b0849023ebff4259b2
DAFFA-0 <awihanga69@gmail.com>
2025-06-29 12:01:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Put the session-id here.", // ඔයාගේ session id එක දාන්න
+SESSION_ID: process.env.SESSION_ID || "IK~hT8mmZya#DhxZC_lTGUFLP9X8FFR-AOCfBpgiitGLIIGl7RwClNk", // ඔයාගේ session id එක දාන්න
 MONGODB: process.env.MONGODB || "mongodb://mongo:UhARYigGUaPXvFIdQOwjPnrzktsxskIm@mainline.proxy.rlwy.net:23844", // ඔයාගේ mongodb url එක දාන්න
-ALIVE_IMG: process.env.ALIVE_IMG || "YOUR PHOTO URL",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://ibb.co/KjQqQWL7",
 BOT_NAME: process.env.BOT_NAME || "MD",
 LANG: process.env.BOT_LANG || 'EN' ,
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
